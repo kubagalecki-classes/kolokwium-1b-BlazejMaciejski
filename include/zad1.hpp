9b4c5a778821bf9edfd3d971415de9baa341a55d
BlazejMaciejski <56317717+BlazejMaciejski@users.noreply.github.com>
2021-01-15 08:34:20 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -7,3 +7,22 @@
 #include <string>
 
 // tutaj klasa Artysta
+class Artysta
+{
+public:
+    const std::string& getPseudonim() const { return pseudonim; }
+    unsigned           getSluchacze() const { return liczbaSluchaczy; }
+    Artysta(const std::string& _pseudonim, unsigned _liczbaSluchaczy)
+        : pseudonim(_pseudonim), liczbaSluchaczy(_liczbaSluchaczy)
+    {}
+    Artysta() : pseudonim("Anonim"), liczbaSluchaczy(0) {}
+
+    void graj(std::ostream& stream)
+    {
+        stream << pseudonim + ": " + std::to_string(liczbaSluchaczy);
+    }
+
+private:
+    std::string pseudonim;
+    unsigned    liczbaSluchaczy;
+}
