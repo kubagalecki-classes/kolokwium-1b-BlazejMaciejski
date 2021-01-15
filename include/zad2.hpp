6c673fbc39a5b4c9ac53d02c2a091de610168aa7
BlazejMaciejski <56317717+BlazejMaciejski@users.noreply.github.com>
2021-01-15 09:44:49 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -5,28 +5,38 @@
 #include <cctype>
 
 // tutaj klasy PopGwiazda, RapGwiazda oraz funkcja stworzArtyste
-class PopGwiazda : Artysta
+class PopGwiazda : public Artysta
 {
+public:
     PopGwiazda(const std::string& _pseudonim, unsigned _liczbaSluchaczy)
-        : pseudonim(_pseudonim), liczbaSluchaczy(_liczbaSluchaczy)
-    {}
+    {
+        this->pseudonim       = _pseudonim;
+        this->liczbaSluchaczy = _liczbaSluchaczy;
+    }
+
+    void graj(std::ostream& stream) const { stream << "PopGwiazda: " << getPseudonim(); }
 };
 
-class RapGwiazda : Artysta
+class RapGwiazda : public Artysta
 {
+public:
     RapGwiazda(const std::string& _pseudonim, unsigned _liczbaSluchaczy)
-        : pseudonim(_pseudonim), liczbaSluchaczy(_liczbaSluchaczy)
-    {}
+    {
+        this->pseudonim       = _pseudonim;
+        this->liczbaSluchaczy = _liczbaSluchaczy;
+    }
+
+    void graj(std::ostream& stream) const { stream << "RapGwiazda: " << getPseudonim(); }
 };
 
 Artysta* stworzArtyste(const std::string& str)
 {
-    if (str.front == std::toupper(str.front)) 
+    if (str.front() == std::toupper(str.front()))
     {
         return new PopGwiazda("BTS", 1234);
     }
     else
     {
-        resturn new RapGwiazda(Ye, 4321);
+        return new RapGwiazda("Ye", 4321);
     }
 }
